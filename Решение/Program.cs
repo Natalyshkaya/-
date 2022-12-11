138c219676a6622cc3440e9bb9e3c7db04eabb83
Nataly <naty.sh@mail.ru>
2022-12-11 19:12:21 +0500

    добавлена программа

---
+++ Решение/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] CreateMassive(int n)
+{
+    string[] arrayText = new string[n];
+    for (int i = 0; i < arrayText.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} строку");
+        arrayText[i] = Console.ReadLine();
+    }
+    Console.WriteLine("Введенный массив: ");
+    for (int i = 0; i < arrayText.Length; i++)
+    {
+        Console.Write(arrayText[i] + " ");
+    }
+    return arrayText;
+}
+
+void NewMassive(string[] arrayText)
+{
+string[] arrayTextNew = new string[arrayText.Length];
+int count = 0;
+    for (int i = 0; i < arrayText.Length; i++)
+    {
+    if(arrayText[i].Length <= 3)
+        {
+        arrayTextNew[count] = arrayText[i];
+        Console.Write(arrayTextNew[count] + " ");
+        count++;
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк массива");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] arrayText = CreateMassive(n);
+Console.WriteLine();
+NewMassive(arrayText);
